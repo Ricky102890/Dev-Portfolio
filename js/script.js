f718f2c815a02f122050bdc45ca62b6dc6e94e49
CJAX.Dev <jackson102890@gmail.com>
2025-06-04 20:53:16 -0600

    Add Some Content

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function navSlide() {
     const navItem = document.querySelector('.nav_item');
     const navMenu = document.querySelector('.nav_menu');
-    
+    const navLinks = document.querySelector('.nav_links');
     navItem.addEventListener('click', () => {
         navMenu.classList.toggle('show');
+        navLinks.classList.toggle('show');
     })
     // navItem.addEventListener('mouseover', () => {
     //     navMenu.classList.toggle('show');

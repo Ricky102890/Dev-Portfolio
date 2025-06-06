eb6713c041066d0e48cd738946c4181f27de05d6
CJAX.Dev <jackson102890@gmail.com>
2025-06-05 19:56:38 -0600

    Add Hero Section and Content
    Added Hero section with some animations along with a navigation rework

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 function navSlide() {
-    const navItem = document.querySelector('.nav_item');
-    const navMenu = document.querySelector('.nav_menu');
-    const navLinks = document.querySelector('.nav_links');
-    navItem.addEventListener('click', () => {
-        navMenu.classList.toggle('show');
-        navLinks.classList.toggle('show');
+    const navItem1 = document.querySelector('.link_box1');
+    const navItem2 = document.querySelector('.link_box2');
+    const navItem3 = document.querySelector('.link_box3');
+    const navMenu1 = document.querySelector('.sub_menu1');
+    const navMenu2 = document.querySelector('.sub_menu2');
+    const navMenu3 = document.querySelector('.sub_menu3');
+    const navLink1 = document.querySelector('sub_link1');
+    const navLink2 = document.querySelector('.sub_link2');
+    const navLink3 = document.querySelector('.sub_link3');
+
+    navItem2.addEventListener('click', () => {
+        navMenu2.classList.toggle('show');
+        navLink2.classList.toggle('show');
+    })
+    navItem3.addEventListener('click', () => {
+        navMenu3.classList.toggle('show');
+        navLink3.classList.toggle('show');
     })
     // navItem.addEventListener('mouseover', () => {
     //     navMenu.classList.toggle('show');
